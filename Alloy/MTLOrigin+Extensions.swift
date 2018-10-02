5b963a00e1d1ed46be7dad2ac9ec5059f3780d8c
s1ddok <siddok@gmail.com>
2018-10-03 00:40:47 +0300

    Tiny extension for MTLOrigin

---
+++ Alloy/MTLOrigin+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  MTLOrigin+Extensions.swift
+//  AIBeauty
+//
+//  Created by Andrey Volodin on 03.10.2018.
+//
+
+import Metal
+
+public extension MTLOrigin {
+    public static let zero = MTLOrigin(x: 0, y: 0, z: 0)
+}
