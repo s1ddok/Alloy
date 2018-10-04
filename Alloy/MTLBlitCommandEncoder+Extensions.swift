dd63f5f40e1ae4ce70af13347a094e7fbe3a2936
s1ddok <siddok@gmail.com>
2018-10-04 14:51:54 +0300

    Handy extension for texture copying

---
+++ Alloy/MTLBlitCommandEncoder+Extensions.swift
@@ -0,0 +1,35 @@
+//
+//  MTLBlitCommandEncoder+Extensions.swift
+//  AIBeauty
+//
+//  Created by Andrey Volodin on 03.10.2018.
+//
+
+import Metal.MTLBlitCommandEncoder
+
+public extension MTLBlitCommandEncoder {
+    public func copy(region: MTLRegion, from texture: MTLTexture,
+                     to targetOrigin: MTLOrigin, of targetTexture: MTLTexture,
+                     sourceSlice: Int = 0, sourceLevel: Int = 0,
+                     destinationSlice: Int = 0, destionationLevel: Int = 0) {
+        self.copy(from: texture,
+                  sourceSlice: sourceSlice, sourceLevel: sourceLevel,
+                  sourceOrigin: region.origin, sourceSize: region.size,
+                  to: targetTexture,
+                  destinationSlice: destinationSlice, destinationLevel: destionationLevel,
+                  destinationOrigin: targetOrigin)
+    }
+    
+    public func copy(texture: MTLTexture,
+                     to targetOrigin: MTLOrigin, of targetTexture: MTLTexture,
+                     sourceSlice: Int = 0, sourceLevel: Int = 0,
+                     destinationSlice: Int = 0, destionationLevel: Int = 0) {
+        let region = texture.region
+        self.copy(from: texture,
+                  sourceSlice: sourceSlice, sourceLevel: sourceLevel,
+                  sourceOrigin: region.origin, sourceSize: region.size,
+                  to: targetTexture,
+                  destinationSlice: destinationSlice, destinationLevel: destionationLevel,
+                  destinationOrigin: targetOrigin)
+    }
+}
