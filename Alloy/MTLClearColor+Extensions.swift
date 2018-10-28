470071c724200330ba79cc552846fff7872e1f8c
s1ddok <siddok@gmail.com>
2018-10-29 00:39:24 +0300

    create new files for prototypes

---
+++ Alloy/MTLClearColor+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  MTLClearColor+Extensions.swift
+//  Alloy
+//
+//  Created by Andrey Volodin on 29.10.2018.
+//
+
+import Foundation
---
+++ Alloy/MTLOffscreenRenderer.swift
@@ -0,0 +1,8 @@
+//
+//  MTLOffscreenRenderer.swift
+//  Alloy
+//
+//  Created by Andrey Volodin on 28.10.2018.
+//
+
+import Foundation
