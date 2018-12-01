15aa0912457da15ba408691f3bbfba07130e9d0b
s1ddok <siddok@gmail.com>
2018-12-02 00:43:51 +0300

    add MTLCommandBuffer extensions

---
+++ Alloy/MTLCommandBuffer+Extensions.swift
@@ -0,0 +1,49 @@
+//
+//  MTLCommandBuffer+Extensions.swift
+//  AIBeauty
+//
+//  Created by Andrey Volodin on 02/12/2018.
+//
+
+import Metal
+
+public extension MTLCommandBuffer {
+    @available(iOS 12.0, *)
+    public func compute(dispatch: MTLDispatchType,
+                        _ commands: (MTLComputeCommandEncoder) -> Void) {
+        guard let encoder = self.makeComputeCommandEncoder(dispatchType: dispatch)
+        else { return }
+        
+        commands(encoder)
+        
+        encoder.endEncoding()
+    }
+    
+    public func compute(_ commands: (MTLComputeCommandEncoder) -> Void) {
+        guard let encoder = self.makeComputeCommandEncoder()
+        else { return }
+        
+        commands(encoder)
+        
+        encoder.endEncoding()
+    }
+    
+    public func blit(_ commands: (MTLBlitCommandEncoder) -> Void) {
+        guard let encoder = self.makeBlitCommandEncoder()
+        else { return }
+        
+        commands(encoder)
+        
+        encoder.endEncoding()
+    }
+    
+    public func render(descriptor: MTLRenderPassDescriptor,
+                       _ commands: (MTLRenderCommandEncoder) -> Void) {
+        guard let encoder = self.makeRenderCommandEncoder(descriptor: descriptor)
+        else { return }
+        
+        commands(encoder)
+        
+        encoder.endEncoding()
+    }
+}
