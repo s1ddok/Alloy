eb1a86120c7543b78b7e248d52f06ec59ae02a4a
s1ddok <siddok@gmail.com>
2019-01-18 16:39:15 +0300

    introduce mtlbuffer extensions

---
+++ Alloy/MTLBuffer+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  MTLBuffer+Extensions.swift
+//  Adjuster
+//
+//  Created by Andrey Volodin on 18/01/2019.
+//
+
+import Metal
+
+public extension MTLBuffer {
+    public func copy(to other: MTLBuffer, offset: Int = 0) {
+        memcpy(other.contents() + offset, self.contents(), self.length)
+    }
+}
