7466331554166684aa0daeb5b64b01cee2eafe8d
s1ddok <siddok@gmail.com>
2019-01-26 19:55:08 +0300

    add demo app

---
+++ Demo/DemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Andrey Volodin on 21/01/2019.
+//  Copyright © 2019 avolodin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
