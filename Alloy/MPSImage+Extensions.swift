779066b3f3d5ac0744fccf22d0ac41e6bde38b40
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-12 14:44:49 +0300

    ‘self.’ style fix

--- Alloy/MPSImage+Extensions.swift
+++ Alloy/MPSImage+Extensions.swift
@@ -16,7 +16,7 @@ public extension MPSImage {
     ///
     /// - Returns: Array of floats containing each pixel of MPSImage's texture.
     func toFloatArray() -> [Float]? {
-        switch pixelFormat {
+        switch self.pixelFormat {
         case .r16Float, .rg16Float, .rgba16Float:
             guard var outputFloat16 = convert(initial: Float16(0)) else { return nil }
             return float16to32(&outputFloat16, count: outputFloat16.count)
@@ -34,28 +34,28 @@ public extension MPSImage {
         guard self.texture.storageMode == .shared || self.texture.storageMode == .managed else { return nil }
         #endif
 
-        let numSlices = (featureChannels + 3) / 4
+        let numSlices = (self.featureChannels + 3) / 4
 
         /// If the number of channels is not a multiple of 4, we may need to add
         /// padding. For 1 and 2 channels we don't need padding.
-        let channelsPlusPadding = (featureChannels < 3) ? featureChannels : numSlices * 4
+        let channelsPlusPadding = (self.featureChannels < 3) ? self.featureChannels : numSlices * 4
 
         /// How many elements we need to copy over from each pixel in a slice.
         /// For 1 channel it's just 1 element (R); for 2 channels it is 2 elements
         /// (R+G), and for any other number of channels it is 4 elements (RGBA).
-        let numComponents = (featureChannels < 3) ? featureChannels : 4
+        let numComponents = (self.featureChannels < 3) ? self.featureChannels : 4
 
         /// Allocate the memory for the array. If batching is used, then we need to
         /// copy numSlices slices for each image in the batch.
-        let count = width * height * channelsPlusPadding * numberOfImages
+        let count = self.width * self.height * channelsPlusPadding * self.numberOfImages
         var output = [T](repeating: initial, count: count)
 
         let region = MTLRegion(origin: MTLOrigin(x: 0, y: 0, z: 0),
-                               size: MTLSize(width: width, height: height, depth: 1))
+                               size: MTLSize(width: self.width, height: self.height, depth: 1))
 
-        for i in 0 ..< numSlices * numberOfImages {
-            texture.getBytes(&(output[width * height * numComponents * i]),
-                             bytesPerRow: width * numComponents * MemoryLayout<T>.size,
+        for i in 0 ..< numSlices * self.numberOfImages {
+            self.texture.getBytes(&(output[self.width * self.height * numComponents * i]),
+                             bytesPerRow: self.width * numComponents * MemoryLayout<T>.self.size,
                              bytesPerImage: 0,
                              from: region,
                              mipmapLevel: 0,
