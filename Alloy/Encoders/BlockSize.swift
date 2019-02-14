403fb9ac010defdeb0fc62bc3d909c5f597aecde
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-14 15:31:43 +0300

    add common BlockSize struct

---
+++ Alloy/Encoders/BlockSize.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Alloy
+//
+//  Created by Eugene Bokhan on 14/02/2019.
+//
+
+import Foundation
+
+public struct BlockSize {
+    var width: UInt16
+    var height: UInt16
+
+    public init(width: UInt16, height: UInt16) {
+        self.width = width
+        self.height = height
+    }
+}
