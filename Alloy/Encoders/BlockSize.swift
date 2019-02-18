b599043dc516925f45c52ea5d55afa262a9a9ac4
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-18 15:28:57 +0300

    make BlockSize properties public

--- Alloy/Encoders/BlockSize.swift
+++ Alloy/Encoders/BlockSize.swift
@@ -8,8 +8,8 @@
 import Foundation
 
 public struct BlockSize {
-    var width: UInt16
-    var height: UInt16
+    public var width: UInt16
+    public var height: UInt16
 
     public init(width: UInt16, height: UInt16) {
         self.width = width
