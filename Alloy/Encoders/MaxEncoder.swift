b5c5605bbfdcb512deff750ef66f7fa2f0c371cb
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-19 13:29:02 +0300

    replace vanilla functions with Alloy’s ones

--- Alloy/Encoders/MaxEncoder.swift
+++ Alloy/Encoders/MaxEncoder.swift
@@ -19,38 +19,24 @@ public class MaxEncoder {
     let pipelineState: MTLComputePipelineState
 
     public init(library: MTLLibrary) throws {
-        guard
-            let function = library.makeFunction(name: MaxEncoder.functionName)
-        else { throw Errors.functionCreationFailed }
-        let pipelineState = try library.device.makeComputePipelineState(function: function)
-        self.pipelineState = pipelineState
+        self.pipelineState = try library.computePipelineState(function: MaxEncoder.functionName)
     }
 
     public func encode(inputTexture: MTLTexture,
                        blockSize: BlockSize,
                        resultBuffer: MTLBuffer,
                        in commandBuffer: MTLCommandBuffer) throws {
-        guard resultBuffer.storageMode == .shared
-        else { throw Errors.invalidBufferStorageMode }
-
         commandBuffer.compute { (encoder) in
             encoder.setTexture(inputTexture, index: 0)
             encoder.set(blockSize, at: 0)
             encoder.setBuffer(resultBuffer,
                               offset: 0,
                               index: 1)
-
-            let threadsPerThreadgroup = self.pipelineState.max2dThreadgroupSize
-
-            // We have to dispatch only one threadgroup so all threads can share a memory
-            let threadgroupsPerGrid = MTLSize(width: 1, height: 1, depth: 1)
-
-            encoder.setThreadgroupMemoryLength(threadsPerThreadgroup.width * threadsPerThreadgroup.height * 4 * MemoryLayout<Float16>.stride,
+            let threadgroupSize = self.pipelineState.max2dThreadgroupSize
+            encoder.setThreadgroupMemoryLength(threadgroupSize.width * threadgroupSize.height * 4 * MemoryLayout<Float16>.stride,
                                                index: 0)
-
-            encoder.setComputePipelineState(self.pipelineState)
-            encoder.dispatchThreadgroups(threadgroupsPerGrid,
-                                         threadsPerThreadgroup: threadsPerThreadgroup)
+            encoder.dispatch2d(state: self.pipelineState,
+                               covering: MTLSize(width: 1, height: 1, depth: 1))
         }
     }
 
