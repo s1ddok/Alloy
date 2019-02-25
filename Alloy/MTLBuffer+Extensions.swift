0b27f5924b0b0acdd187f7acc59f80c24a90b06f
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-25 18:50:47 +0300

    style fix

--- Alloy/MTLBuffer+Extensions.swift
+++ Alloy/MTLBuffer+Extensions.swift
@@ -13,20 +13,23 @@ public extension MTLBuffer {
     }
 
     func pointer<T>(of type: T.Type) -> UnsafeMutablePointer<T>? {
-        guard self.isAccessibleOnCPU else { return nil }
+        guard self.isAccessibleOnCPU
+        else { return nil }
         let bindedPointer = self.contents().assumingMemoryBound(to: type)
         return bindedPointer
     }
 
     func bufferPointer<T>(of type: T.Type, capacity: Int) -> UnsafeBufferPointer<T>? {
-        guard let startPointer = self.pointer(of: type) else { return nil }
+        guard let startPointer = self.pointer(of: type)
+        else { return nil }
         let bufferPointer = UnsafeBufferPointer(start: startPointer,
                                                 count: capacity)
         return bufferPointer
     }
 
-    func array<T>(of type: T.Type, capacity: Int) -> [T]? {
-        guard let bufferPointer = self.bufferPointer(of: type, capacity: capacity) else { return nil }
+    func array<T>(of type: T.Type, count: Int) -> [T]? {
+        guard let bufferPointer = self.bufferPointer(of: type, capacity: count)
+        else { return nil }
         let valueArray = Array(bufferPointer)
         return valueArray
     }
