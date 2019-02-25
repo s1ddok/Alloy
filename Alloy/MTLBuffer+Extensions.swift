4fcc3fc279ba605f119ca4bc9ede9aa221567397
Eugene <eugenebokhan@protonmail.com>
2019-02-25 18:54:33 +0300

    add MTLBuffer extensions (#10)
    * add MTLBuffer extensions
    
    * style fix
    
    * replace capacity with count

--- Alloy/MTLBuffer+Extensions.swift
+++ Alloy/MTLBuffer+Extensions.swift
@@ -11,4 +11,26 @@ public extension MTLBuffer {
     func copy(to other: MTLBuffer, offset: Int = 0) {
         memcpy(other.contents() + offset, self.contents(), self.length)
     }
+
+    func pointer<T>(of type: T.Type) -> UnsafeMutablePointer<T>? {
+        guard self.isAccessibleOnCPU
+        else { return nil }
+        let bindedPointer = self.contents().assumingMemoryBound(to: type)
+        return bindedPointer
+    }
+
+    func bufferPointer<T>(of type: T.Type, count: Int) -> UnsafeBufferPointer<T>? {
+        guard let startPointer = self.pointer(of: type)
+        else { return nil }
+        let bufferPointer = UnsafeBufferPointer(start: startPointer,
+                                                count: count)
+        return bufferPointer
+    }
+
+    func array<T>(of type: T.Type, count: Int) -> [T]? {
+        guard let bufferPointer = self.bufferPointer(of: type, count: count)
+        else { return nil }
+        let valueArray = Array(bufferPointer)
+        return valueArray
+    }
 }
