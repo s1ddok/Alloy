1f110f105c47a09f148e8ba5b1232216db26923e
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-02-25 17:53:26 +0300

    remove watchOS

--- MTLResource+Extensions.swift
+++ MTLResource+Extensions.swift
@@ -10,7 +10,7 @@ import Metal
 public extension MTLResource {
 
     var isAccessibleOnCPU: Bool {
-        #if os(iOS) || os(tvOS) || os(watchOS)
+        #if os(iOS) || os(tvOS)
         return self.storageMode == .shared
         #elseif os(OSX)
         return self.storageMode == .managed
