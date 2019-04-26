01f3dc79d028e361da114cc87a56da8973030e0d
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-04-26 13:08:40 +0300

    Make floats packed

--- Alloy/Shaders/ShaderStructures.h
+++ Alloy/Shaders/ShaderStructures.h
@@ -11,15 +11,15 @@
 #include <simd/simd.h>
 
 typedef struct Rectangle {
-    vector_float2 topLeft;
-    vector_float2 bottomLeft;
-    vector_float2 topRight;
-    vector_float2 bottomRight;
+    packed_float2 topLeft;
+    packed_float2 bottomLeft;
+    packed_float2 topRight;
+    packed_float2 bottomRight;
 } Rectangle;
 
 typedef struct Line {
-    vector_float2 startPoint;
-    vector_float2 endPoint;
+    packed_float2 startPoint;
+    packed_float2 endPoint;
     float width;
 } Line;
 
