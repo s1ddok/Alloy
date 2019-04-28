a76f13f4cbe7015076cc6fc6f47d96265989f4cd
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-04-28 09:50:11 +0300

    add more blending modes

--- Alloy/MTLRenderPipelineColorAttachmentDescriptor+Extensions.swift
+++ Alloy/MTLRenderPipelineColorAttachmentDescriptor+Extensions.swift
@@ -9,24 +9,75 @@ import Metal
 
 public extension MTLRenderPipelineColorAttachmentDescriptor {
 
-    enum BlendingType {
-        case none, add
+    /// Blend Mode Options
+    enum BlendingMode {
+        /// Disabled blending mode. Use this with fully opaque surfaces for extra performance.
+        case none
+        /// Regular alpha blending.
+        case alpha
+        /// Pre-multiplied alpha blending. (This is usually the default)
+        case premultipliedAlpha
+        /// Additive blending. (Similar to PhotoShop's linear dodge mode)
+        case add
+        /// Multiply blending mode. (Similar to PhotoShop's burn mode)
+        case multiply
+        /// A (better) multiply blending mode.
+        case multiplicative
+        /// A (better) add mode.
+        case addWithAlpha
+        /// Similar to PhotoShop's screen mode.
+        case screen
+        /// Similar to PhotoShop's dodge mode.
+        case dodge
     }
 
-    func setup(blending: BlendingType) {
+    /// Setup a certain blending mode.
+    ///
+    /// - Parameter blending: Blending mode.
+    func setup(blending: BlendingMode) {
+        // Default
+        self.rgbBlendOperation = .add
+        self.sourceRGBBlendFactor = .one
+        self.destinationRGBBlendFactor = .zero
+        self.alphaBlendOperation = .add
+        self.sourceAlphaBlendFactor = .one
+        self.destinationAlphaBlendFactor = .zero
+
         switch blending {
         case .none:
             self.isBlendingEnabled = false
-        case .add:
+        case .alpha:
             self.isBlendingEnabled = true
-
-            self.rgbBlendOperation = .add
             self.sourceRGBBlendFactor = .sourceAlpha
             self.destinationRGBBlendFactor = .oneMinusSourceAlpha
-
-            self.alphaBlendOperation = .add
-            self.sourceAlphaBlendFactor = .sourceAlpha
-            self.destinationAlphaBlendFactor = .oneMinusSourceAlpha
+        case .premultipliedAlpha:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .one
+            self.destinationRGBBlendFactor = .oneMinusSourceAlpha
+        case .add:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .one
+            self.destinationRGBBlendFactor = .one
+        case .multiply:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .destinationColor
+            self.destinationRGBBlendFactor = .zero
+        case .multiplicative:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .destinationColor
+            self.destinationRGBBlendFactor = .oneMinusSourceAlpha
+        case .addWithAlpha:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .sourceAlpha
+            self.destinationRGBBlendFactor = .zero
+        case .screen:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .oneMinusDestinationColor
+            self.destinationRGBBlendFactor = .one
+        case .dodge:
+            self.isBlendingEnabled = true
+            self.sourceRGBBlendFactor = .oneMinusSourceAlpha
+            self.destinationRGBBlendFactor = .one
         }
     }
 
