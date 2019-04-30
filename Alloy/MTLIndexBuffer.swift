cdfc17682af91a21450103464d0a4166b7612b2c
s1ddok <siddok@gmail.com>
2019-04-30 17:45:33 +0300

    introduce MTLIndexBuffer

---
+++ Alloy/MTLIndexBuffer.swift
@@ -0,0 +1,40 @@
+//
+//  MTLIndexBuffer.swift
+//  Alloy
+//
+//  Created by Andrey Volodin on 30/04/2019.
+//
+
+import Metal
+
+public class MTLIndexBuffer {
+    public enum Errors: Error {
+        case allocationFailed
+    }
+
+    public let buffer: MTLBuffer
+    public let count: Int
+    public let type: MTLIndexType
+
+    public init(device: MTLDevice, _ indexArray: [UInt16], options: MTLResourceOptions = []) throws {
+        guard let allocatedBuffer = device.makeBuffer(bytes: indexArray,
+                                                      length: indexArray.count * MemoryLayout<UInt16>.stride,
+                                                      options: options)
+        else { throw Errors.allocationFailed }
+
+        self.buffer = allocatedBuffer
+        self.count = indexArray.count
+        self.type = .uint16
+    }
+
+    public init(device: MTLDevice, _ indexArray: [UInt32], options: MTLResourceOptions = []) throws {
+        guard let allocatedBuffer = device.makeBuffer(bytes: indexArray,
+                                                      length: indexArray.count * MemoryLayout<UInt32>.stride,
+                                                      options: options)
+        else { throw Errors.allocationFailed }
+
+        self.buffer = allocatedBuffer
+        self.count = indexArray.count
+        self.type = .uint32
+    }
+}
