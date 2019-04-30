12d2dd6d0051006d15ed173da7e77f3ae19d55b5
s1ddok <siddok@gmail.com>
2019-04-30 17:46:01 +0300

    introduce extensions to make draw calls via Alloy's MTLIndexBuffer

--- Alloy/MTLRenderCommandEncoder+Extensions.swift
+++ Alloy/MTLRenderCommandEncoder+Extensions.swift
@@ -49,4 +49,34 @@ public extension MTLRenderCommandEncoder {
                                 range: startIndex..<(startIndex + buffers.count))
     }
 
+    func drawIndexedPrimitives(type: MTLPrimitiveType,
+                               indexBuffer: MTLIndexBuffer,
+                               instanceCount: Int = 1) {
+        self.drawIndexedPrimitives(type: type,
+                                   indexCount: indexBuffer.count,
+                                   indexType: indexBuffer.type,
+                                   indexBuffer: indexBuffer.buffer,
+                                   indexBufferOffset: 0,
+                                   instanceCount: instanceCount)
+    }
+
+    func drawIndexedPrimitives(type: MTLPrimitiveType,
+                               indexBuffer: MTLIndexBuffer,
+                               offset: Int,
+                               count: Int,
+                               instanceCount: Int = 1) {
+        #if DEBUG
+        guard count + offset <= indexBuffer.count else {
+            fatalError("Requested index count exceeds provided buffer's length")
+        }
+        #endif
+
+        self.drawIndexedPrimitives(type: type,
+                                   indexCount: count,
+                                   indexType: indexBuffer.type,
+                                   indexBuffer: indexBuffer.buffer,
+                                   indexBufferOffset: offset,
+                                   instanceCount: instanceCount)
+    }
+
 }
