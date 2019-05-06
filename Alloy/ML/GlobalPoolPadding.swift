3ac477b7c19b8eaba9ea97cdb27ec3bbfdbeab77
s1ddok <siddok@gmail.com>
2019-05-06 14:07:43 +0300

    introduce global padding

---
+++ Alloy/ML/GlobalPoolPadding.swift
@@ -0,0 +1,49 @@
+//
+//  GlobalPoolPadding.swift
+//  Alloy
+//
+//  Created by Andrey Volodin on 05/05/2019.
+//
+
+import MetalPerformanceShaders
+
+@available(iOS 11.0, macOS 10.13, *)
+public class GlobalPoolPadding: NSObject, MPSNNPadding {
+
+    public override init() {}
+
+    // MARK: MPSNNPadding
+
+    public func paddingMethod() -> MPSNNPaddingMethod {
+        return .custom
+    }
+
+    public func label() -> String {
+        return "PyTorch Global Pool Padding rule"
+    }
+
+    public func destinationImageDescriptor(forSourceImages sourceImages: [MPSImage],
+                                           sourceStates: [MPSState]?,
+                                           for kernel: MPSKernel,
+                                           suggestedDescriptor inDescriptor: MPSImageDescriptor) -> MPSImageDescriptor {
+        inDescriptor.width = 1
+        inDescriptor.height = 1
+
+        return inDescriptor
+    }
+
+    // MARK: NSCoding
+
+    public static var supportsSecureCoding: Bool {
+        return false
+    }
+
+    public func encode(with aCoder: NSCoder) {
+        fatalError("NSCoding is not supported yet")
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("NSCoding is not supported yet")
+    }
+
+}
