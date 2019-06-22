8fd3618cd9cdb4b83fe75e8d38799b4590b18217
s1ddok <siddok@gmail.com>
2019-06-22 12:40:05 +0300

    migrate to feature API

--- Alloy/Encoders/TextureCopy.swift
+++ Alloy/Encoders/TextureCopy.swift
@@ -9,21 +9,14 @@ import Metal
 
 @available(iOS 11.3, tvOS 11.3, macOS 10.13, *)
 final public class TextureCopy {
-    public let library: MTLLibrary
-
     public var inputTexture: MTLTexture!
     public var outputTexture: MTLTexture!
 
-    private let pipelineState: MTLComputePipelineState
+    public let pipelineState: MTLComputePipelineState
     private let deviceSupportsNonuniformThreadgroups: Bool
 
     public init(library: MTLLibrary) throws {
-        self.library = library
-        #if os(iOS) || os(tvOS)
-        self.deviceSupportsNonuniformThreadgroups = library.device.supportsFeatureSet(.iOS_GPUFamily4_v1)
-        #elseif os(macOS)
-        self.deviceSupportsNonuniformThreadgroups = library.device.supportsFeatureSet(.macOS_GPUFamily1_v3)
-        #endif
+        self.deviceSupportsNonuniformThreadgroups = library.device.supports(feature: .nonUniformThreadgroups)
         let constantValues = MTLFunctionConstantValues()
         var dispatchFlag = self.deviceSupportsNonuniformThreadgroups
         constantValues.setConstantValue(&dispatchFlag,
