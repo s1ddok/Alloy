5c670198c0e3cbb141a2a4aced80556e380aeb44
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-09-03 14:06:29 +0300

    add PixelFormatScalarTypes

---
+++ Alloy/PixelFormatScalarTypes.swift
@@ -0,0 +1,12 @@
+//
+//  PixelFormatScalarTypes.swift
+//  Alloy
+//
+//  Created by Eugene Bokhan on 02/09/2019.
+//
+
+import Foundation
+
+public enum PixelFormatScalarType: String {
+    case float, half, ushort, short, uint, int, unknown
+}
