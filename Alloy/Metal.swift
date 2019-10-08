988b08d710db8a81c896b42326434fb7b160ea70
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-10-08 15:34:21 +0300

    add #if check
    MTLCopyAllDevices symbols are not seen in iOS SKD at all, so @available(iOS, unavailable) doesn't work.

--- Alloy/Metal.swift
+++ Alloy/Metal.swift
@@ -13,10 +13,12 @@ public final class Metal {
     
     public static let device: MTLDevice! = MTLCreateSystemDefaultDevice()
     
+    #if os(macOS) || targetEnvironment(macCatalyst)
     @available(macOS 10.11, macCatalyst 13.0, *)
     public static let lowPowerDevice: MTLDevice? = {
         return MTLCopyAllDevices().first { $0.isLowPower }
     }()
+    #endif // os(macOS) || targetEnvironment(macCatalyst)
     
     public static var isAvailable: Bool {
         return Metal.device != nil
