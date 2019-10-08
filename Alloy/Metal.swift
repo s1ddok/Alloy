d65fdd121f0d1c514c03c2fba672579b87666f11
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-10-08 14:12:52 +0300

    remove unnecessary #if

--- Alloy/Metal.swift
+++ Alloy/Metal.swift
@@ -13,12 +13,10 @@ public final class Metal {
     
     public static let device: MTLDevice! = MTLCreateSystemDefaultDevice()
     
-    #if os(macOS)
     @available(macOS 10.11, macCatalyst 13.0, *)
     public static let lowPowerDevice: MTLDevice? = {
         return MTLCopyAllDevices().first { $0.isLowPower }
     }()
-    #endif // os(macOS)
     
     public static var isAvailable: Bool {
         return Metal.device != nil
