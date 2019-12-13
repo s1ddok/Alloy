ef999166fc4bf130565fb5e8f674075957aa7cef
Eugene Bokhan <eugenebokhan@protonmail.com>
2019-12-13 16:06:29 +0300

    fix errors in MTLIndexBuffer

--- Alloy/MTLIndexBuffer.swift
+++ Alloy/MTLIndexBuffer.swift
@@ -13,22 +13,26 @@ public class MTLIndexBuffer {
     public let count: Int
     public let type: MTLIndexType
 
-    public init(device: MTLDevice, indexArray: [UInt16], options: MTLResourceOptions = []) throws {
+    public init(device: MTLDevice,
+                indexArray: [UInt16],
+                options: MTLResourceOptions = []) throws {
         guard let allocatedBuffer = device.makeBuffer(bytes: indexArray,
                                                       length: indexArray.count * MemoryLayout<UInt16>.stride,
                                                       options: options)
-        else { throw MetalError.MTLBufferError.allocationFailed }
+        else { throw MetalError.MTLDeviceError.bufferCreationFailed }
 
         self.buffer = allocatedBuffer
         self.count = indexArray.count
         self.type = .uint16
     }
 
-    public init(device: MTLDevice, indexArray: [UInt32], options: MTLResourceOptions = []) throws {
+    public init(device: MTLDevice,
+                indexArray: [UInt32],
+                options: MTLResourceOptions = []) throws {
         guard let allocatedBuffer = device.makeBuffer(bytes: indexArray,
                                                       length: indexArray.count * MemoryLayout<UInt32>.stride,
                                                       options: options)
-        else { throw MetalError.MTLBufferError.allocationFailed }
+        else { throw MetalError.MTLDeviceError.bufferCreationFailed }
 
         self.buffer = allocatedBuffer
         self.count = indexArray.count
