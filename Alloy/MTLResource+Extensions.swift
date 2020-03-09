5baa2cd0569175299259589dbde9b355725500fb
Eugene Bokhan <eugenebokhan@protonmail.com>
2020-03-09 13:45:53 +0300

    fix isAccessibleOnCPU

--- Alloy/MTLResource+Extensions.swift
+++ Alloy/MTLResource+Extensions.swift
@@ -10,9 +10,9 @@ import Metal
 public extension MTLResource {
 
     var isAccessibleOnCPU: Bool {
-        #if os(iOS) || os(tvOS)
+        #if (os(iOS) && !targetEnvironment(macCatalyst)) || os(tvOS)
         return self.storageMode == .shared
-        #elseif os(macOS)
+        #elseif os(macOS) || (os(iOS) && targetEnvironment(macCatalyst))
         return self.storageMode == .managed || self.storageMode == .shared
         #endif
     }
