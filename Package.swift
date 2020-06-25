c4dac314ed84685a52b5dbcdc2db92c35a0b593e
Andrey Volodin <siddok@gmail.com>
2020-06-25 22:18:33 +0300

    add targets

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "Alloy",
-            targets: ["Alloy"]),
+            targets: ["Alloy", "ShadersSharedCode"]),
     ],
     dependencies: [
     ],
