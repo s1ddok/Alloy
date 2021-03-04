c065328af0f361c0cd6fb43c1008b8bd0da401fa
eugene <eugenebokhan@icloud.com>
2021-03-04 19:12:15 +0300

    style fixes

--- Sources/Alloy/Core/Float16.swift
+++ Sources/Alloy/Core/Float16.swift
@@ -14,8 +14,10 @@ public typealias Float16 = UInt16
 ///   - input: A pointer to an array of `Float16`s.
 ///   - count: Number of elements in the array.
 /// - Returns: An array of regular Swift `Float`s.
-public func float16to32(_ input: UnsafeMutableRawPointer, count: Int) -> [Float]? {
-    let output = [Float](repeating: 0, count: count)
+public func float16to32(_ input: UnsafeMutableRawPointer,
+                        count: Int) -> [Float]? {
+    let output = [Float](repeating: 0,
+                         count: count)
     var data: UnsafeMutableRawPointer? = nil
     output.withUnsafeBytes {
         data = UnsafeMutableRawPointer(mutating: $0.baseAddress)
@@ -45,17 +47,27 @@ public func float16to32(_ input: UnsafeMutableRawPointer, count: Int) -> [Float]
 ///   - input: A pointer to an array of `Float`s.
 ///   - count: Number of elements in the array.
 /// - Returns: An array of `Float16`s.
-public func float32to16(_ input: UnsafeMutablePointer<Float>, count: Int) -> [Float16]? {
-    let output = [Float16](repeating: 0, count: count)
+public func float32to16(_ input: UnsafeMutablePointer<Float>,
+                        count: Int) -> [Float16]? {
+    let output = [Float16](repeating: 0,
+                           count: count)
     var data: UnsafeMutableRawPointer? = nil
     output.withUnsafeBytes {
         data = UnsafeMutableRawPointer(mutating: $0.baseAddress)
     }
 
-    var bufferFloat32 = vImage_Buffer(data: input, height: 1, width: UInt(count), rowBytes: count * 4)
-    var bufferFloat16 = vImage_Buffer(data: data, height: 1, width: UInt(count), rowBytes: count * 2)
+    var bufferFloat32 = vImage_Buffer(data: input,
+                                      height: 1,
+                                      width: UInt(count),
+                                      rowBytes: count * 4)
+    var bufferFloat16 = vImage_Buffer(data: data,
+                                      height: 1,
+                                      width: UInt(count),
+                                      rowBytes: count * 2)
 
-    if vImageConvert_PlanarFtoPlanar16F(&bufferFloat32, &bufferFloat16, 0) != kvImageNoError {
+    if vImageConvert_PlanarFtoPlanar16F(&bufferFloat32,
+                                        &bufferFloat16,
+                                        0) != kvImageNoError {
         return nil
     }
     return output
